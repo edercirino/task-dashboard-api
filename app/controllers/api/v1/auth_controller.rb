a06266464daccd8307bc5378f8cfaf1058ec9674
edercirino <edersantoscirino@gmail.com>
2025-05-12 18:40:51 -0300

    Create: user create endpoint

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,9 +7,24 @@ module Api
         if user&.authenticate(params[:password])
         token = jwt_encode(user_id: user.id)
         render json:
-        { token:, user: { id: user.id, name: user.name, email: user.email } }, status: :ok
+        { token:, user: { id: user.id, name: user.name, email: user.email } },
+        status: :ok
         else
           render json: { error: "Invalid email or password" }, status: :unauthorized
+        end
+      end
+
+      def register
+        user = User.new(user_params)
+
+        if user.save
+          token = jwt_encode(user_id: user.id)
+          render json:
+          { token:, user: { id: user.id, name: user.name, email: user.email } },
+          status: :created
+        else
+          render json:
+          { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
       end
 
@@ -18,6 +33,10 @@ module Api
       def jwt_encode(payload, exp = 24.hours.from_now)
         payload[:exp] = exp.to_i
         JWT.encode(payload, Rails.application.credentials.secret_key_base)
+      end
+
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post "login", to: "auth#login"
+      post "signup", to: "auth#register"
     end
   end
 end
